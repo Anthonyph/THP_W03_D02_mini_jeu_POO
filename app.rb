36c7d4119240ea566ec56e253ebe0e061ac9c1bd
Anthonyph <anthony.phim@gmail.com>
2019-10-15 09:20:18 +0200

    exo 1 fini

--- app.rb
+++ app.rb
@@ -4,20 +4,53 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-def new_player
-player1 = Player.new("Noémie")
-player2 = Player.new("Anthony")
+def new_player1
+  player1 = Player.new("Josiane")
+  return player1
+end
+def new_player2
+  player2 = Player.new("José") 
+  return player2
 end
 
 def presentation
-puts "Avant de commencer, voici l'état de chaque joueur :"
-player1.show_state
-player2.show_state
+  player1 = new_player1
+  player2 = new_player2
+  puts "Voici l'état de chaque joueurs :"
+  player1.show_state
+  player2.show_state
 end
 
-def perform 
-new_player
+def fight_begin
+  player1 = new_player1
+  player2 = new_player2
+  puts " "
+  puts "Passons à la phase d'attaque !"
+  puts " "
+    while player1.life_points > 0 && player2.life_points >0
+    player1.attacks(player2)
+      if player2.life_points <= 0
+      break
+      end
+    player2.attacks(player1)
+    puts " "
+    puts "#######################################################"
+    puts " "
+    puts "Voici l'état de nos braves guerriers : "
+    puts " "
+    player1.show_state
+    player2.show_state
+    puts " "
+    end
+end
+
+
+
+def perform
+new_player1
+new_player2
 presentation
+fight_begin
 end
 
 binding.pry
